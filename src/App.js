66ed73a904fbf26faf783c4214ce2bcc0a46b2b5
Alexander Kolotow <alexkolotow@gmail.com>
2023-11-13 11:26:53 +0100

    solved 2 task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+  <article> 
+  <h1> Does that work?</h1>
+  <p> Some text. </p>
+  </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
 }
